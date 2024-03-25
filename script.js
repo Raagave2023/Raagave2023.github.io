dbdb9e341e496ceae48afca913961c143e5c4cae
Raagave2023 <raagave.13@gmail.com>
2024-03-25 14:17:49 -0400

    init

---
+++ script.js
@@ -0,0 +1,13 @@
+
+(() => {
+  const modelViewer = document.querySelector('#shadow-intensity-demo');
+  const time = performance.now();
+
+//   const animate = (now) => {
+//     modelViewer.shadowIntensity = oscillate(0, 2, 4000, now - time);
+//     requestAnimationFrame(animate);
+//   };
+
+  animate();
+})();
+ 
